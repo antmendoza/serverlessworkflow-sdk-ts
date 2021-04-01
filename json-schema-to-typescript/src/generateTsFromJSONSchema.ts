12a1292656cce898cf466e1e85e92f6bf2cd024a
Antonio Mendoza Pérez <antonio.mendoza@bideaavant.com>
2021-04-01 19:38:49 +0200

    wip

--- json-schema-to-typescript/src/generateTsFromJSONSchema.ts
+++ json-schema-to-typescript/src/generateTsFromJSONSchema.ts
@@ -4,8 +4,10 @@ import {compileFromFile} from 'json-schema-to-typescript'
 async function generate() {
     try {
 
-        writeFileSync('./src/workflow.ts', await compileFromFile('./src/schema/workflow.json',
-            {cwd: './src/schema/'}))
+        writeFileSync('./src/workflow.ts',
+            await compileFromFile('./src/schema/workflow.json',
+                {cwd: './src/schema/'})
+        )
     } catch (error) {
         console.log(error);
     }
