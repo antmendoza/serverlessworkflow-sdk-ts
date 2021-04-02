6a64f83ef2c5df5ffc7ad4bed7fdaa196db87908
Antonio Mendoza Pérez <antonio.mendoza@bideaavant.com>
2021-04-02 14:59:27 +0200

    should generate workflow object from json file

--- spec/base-workflow.spec.ts
+++ spec/base-workflow.spec.ts
@@ -33,9 +33,7 @@ describe("base-workflow", () => {
     });
 
 
-
-
-    xit('should generate workflow object from yaml file', function () {
+    it('should generate workflow object from yaml file', () => {
         const workflow: Workflow = new BaseWorkflow().fromSource(__dirname + "/base-workflow-hello-world.yaml");
 
         expect(workflow.id).toBe("helloworld");
@@ -43,6 +41,64 @@ describe("base-workflow", () => {
         expect(workflow.name).toBe("Hello World Workflow");
         expect(workflow.description).toBe("Inject Hello World");
         expect(workflow.start).toBe("Hello State");
+
+        expect(workflow).toEqual({
+            "id": "helloworld",
+            "version": "1.0",
+            "name": "Hello World Workflow",
+            "description": "Inject Hello World",
+            "start": "Hello State",
+            "states": [
+                {
+                    "name": "Hello State",
+                    "type": "inject",
+                    "data": {
+                        "result": "Hello World!"
+                    },
+                    "end": true
+                }
+            ]
+        })
+
+    });
+
+
+
+    it('should generate workflow object from yaml file', () => {
+        const workflow: Workflow = new BaseWorkflow().fromSource(__dirname + "/base-workflow-hello-world.yml");
+
+        expect(workflow.id).toBe("helloworld");
+        expect(workflow.version).toBe("1.0");
+        expect(workflow.name).toBe("Hello World Workflow");
+        expect(workflow.description).toBe("Inject Hello World");
+        expect(workflow.start).toBe("Hello State");
+
+        expect(workflow).toEqual({
+            "id": "helloworld",
+            "version": "1.0",
+            "name": "Hello World Workflow",
+            "description": "Inject Hello World",
+            "start": "Hello State",
+            "states": [
+                {
+                    "name": "Hello State",
+                    "type": "inject",
+                    "data": {
+                        "result": "Hello World!"
+                    },
+                    "end": true
+                }
+            ]
+        })
+
+    });
+
+
+    it('should throws error if format is not json or yaml', () => {
+        expect(() => {
+            new BaseWorkflow().fromSource(__dirname + "/base-workflow-hello-world.xxx")    }
+        ).toThrow(new Error("File format not supported"));
+
 
     });
 
--- src/base-workflow.ts
+++ src/base-workflow.ts
@@ -1,9 +1,31 @@
+import * as fs from "fs";
 import {PathLike} from "fs";
 import {Workflow} from "./model/workflow";
-import * as fs from "fs";
+import * as yaml from "js-yaml";
 
 export class BaseWorkflow {
-    fromSource(path: PathLike): Workflow {
-        return JSON.parse(fs.readFileSync(path).toLocaleString()) as Workflow;
+
+    fromSource(filePath: PathLike): Workflow {
+
+
+        if (!this.isJSON(filePath) && !this.isYML(filePath)) {
+            throw new Error("File format not supported")
+        }
+
+        const fileAsString = fs.readFileSync(filePath).toLocaleString();
+        if (this.isJSON(filePath)) {
+            return JSON.parse(fileAsString) as Workflow;
+        }
+
+        return yaml.load(fileAsString) as Workflow;
+
+    }
+
+    private isYML(filePath: PathLike) {
+        return filePath.toString().endsWith("yaml") || filePath.toString().endsWith("yml");
+    }
+
+    private isJSON(filePath: PathLike) {
+        return filePath.toString().endsWith("json");
     }
 }
